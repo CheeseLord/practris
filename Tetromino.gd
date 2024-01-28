3c431ff1bbbfd1a65416a284c5b231802fb51f5f
JoelO <joel.ornstein@colorado.edu>
2024-01-27 17:01:31 -0700

    Added blocks to tetromino

--- Tetromino.gd
+++ Tetromino.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var block_scene: PackedScene
+
 # https://strategywiki.org/wiki/Tetris/Rotation_systems#Super_rotation_system
 const ALL_TILES = [
 	[[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0]],  # I
@@ -12,13 +14,22 @@ const ALL_TILES = [
 ]
 
 var tile
-
+var blocks = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	tile = ALL_TILES[randi() % ALL_TILES.size()].duplicate(true)
-
-
+	for r in range(tile.size()):
+		for c in range(tile[r].size()):
+			if tile[r][c]:
+				var block = block_scene.instantiate()
+				var block_size = block.get_size()
+				block.position = Vector2(c * block_size.x, r * block_size.y)
+				print(block.position)
+				blocks.append(block)
+				add_child(block)
+ 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
